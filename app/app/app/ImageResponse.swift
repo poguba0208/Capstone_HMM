f9650e57bdb78e406915966be7cd81cef3e56b11
haeun0529 <h991512@naver.com>
2026-04-21 09:55:56 +0900

    fix(app) : 이미지 업로드 api 데이터 수정

--- app/app/app/ImageResponse.swift
+++ app/app/app/ImageResponse.swift
@@ -1,6 +1,46 @@
+import UIKit
+
 struct ImageResponse: Codable {
     let imageId: Int
     let originalUrl: String
     let processedUrl: String
     let riskScore: Double
+}
+
+func uploadImage(image: UIImage, completion: @escaping (ImageResponse?) -> Void) {
+    guard let url = URL(string: "http://localhost:8080/api/images/upload") else { return }
+    
+    var request = URLRequest(url: url)
+    request.httpMethod = "POST"
+    
+    let boundary = UUID().uuidString
+    request.setValue("multipart/form-data; boundary=\(boundary)", forHTTPHeaderField: "Content-Type")
+    
+    var data = Data()
+    
+    let imageData = image.jpegData(compressionQuality: 0.8)!
+    
+    data.append("--\(boundary)\r\n".data(using: .utf8)!)
+    data.append("Content-Disposition: form-data; name=\"file\"; filename=\"image.jpg\"\r\n".data(using: .utf8)!)
+    data.append("Content-Type: image/jpeg\r\n\r\n".data(using: .utf8)!)
+    data.append(imageData)
+    data.append("\r\n".data(using: .utf8)!)
+    
+    data.append("--\(boundary)\r\n".data(using: .utf8)!)
+    data.append("Content-Disposition: form-data; name=\"option\"\r\n\r\n".data(using: .utf8)!)
+    data.append("strong\r\n".data(using: .utf8)!)
+    
+    data.append("--\(boundary)--\r\n".data(using: .utf8)!)
+    
+    request.httpBody = data
+    
+    URLSession.shared.dataTask(with: request) { data, _, _ in
+        guard let data = data else { return }
+        
+        let result = try? JSONDecoder().decode(ImageResponse.self, from: data)
+        
+        DispatchQueue.main.async {
+            completion(result)
+        }
+    }.resume()
 }
